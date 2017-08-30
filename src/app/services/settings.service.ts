607571dd393bd84601156e4a546a657e1fc93989
Scott Turnbull <streamweaver@gmail.com>
2017-08-29 23:27:51 -0400

    Finished up settings page

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -11,9 +11,17 @@ export class SettingsService {
       disableBalanceOnAdd: true,
       disableBalanceOnEdit: true
     };
+    if (localStorage.getItem('settings') != null) {
+      this.settings = JSON.parse(localStorage.getItem('settings'));
+    }
   }
 
   getSettings(): Settings {
     return this.settings;
+  }
+
+  changeSettings(settings: Settings) {
+    this.settings = settings;
+    localStorage.setItem('settings', JSON.stringify(settings));
   }
 }
